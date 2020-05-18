fec133066656154731b5cb8e5f1d00254378246f
James Folk <james@njligames.com>
2020-05-18 15:38:02 -0400

    updated the header

--- includes/NJLICEngine/NJLICGame.h
+++ includes/NJLICEngine/NJLICGame.h
@@ -27,28 +27,28 @@ class NJLICGame {
     }
     virtual void update(double step) {}
     virtual void render()const {}
-    virtual void resize(unsigned int w, unsigned int h)const {}
+    virtual void resize(unsigned int w, unsigned int h) {}
     virtual void init(int argc, char *argv[]) {}
     virtual void unInit() {}
-    virtual void start()const {}
-    virtual void stop()const {}
+    virtual void start() {}
+    virtual void stop() {}
 
     virtual void mouse(int button, int eventType, float x, float y,
-                       int clicks)const {}
+                       int clicks) {}
     virtual void keyDown(const std::string &keycodeName, bool withCapsLock,
                          bool withControl, bool withShift, bool withAlt,
-                         bool withGui)const {}
+                         bool withGui) {}
     virtual void keyUp(const std::string &keycodeName, bool withCapsLock,
                        bool withControl, bool withShift, bool withAlt,
-                       bool withGui)const {}
+                       bool withGui) {}
     virtual void touch(int touchDevId, int pointerFingerId, int eventType,
-                       float x, float y, float dx, float dy, float pressure)const {}
+                       float x, float y, float dx, float dy, float pressure) {}
 
     virtual void vRCameraRotation(float m11, float m12, float m13, float m21,
                                   float m22, float m23, float m31, float m32,
-                                  float m33)const {}
+                                  float m33) {}
 
-    virtual void vRCameraRotationYPR(float yaw, float pitch, float roll)const {}
+    virtual void vRCameraRotationYPR(float yaw, float pitch, float roll) {}
 
     //        virtual void vRCameraLocation(float x, float y, float z) {}
 
