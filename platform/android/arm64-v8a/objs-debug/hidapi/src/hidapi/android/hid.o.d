4b7e5a2e0a6ea54963c009f5ce19208f3f4f29c9
James Folk <james@njligames.com>
2020-06-19 15:30:37 -0400

    removed objecdt symobols for sdl2

--- platform/android/arm64-v8a/objs-debug/hidapi/src/hidapi/android/hid.o.d
+++
@@ -1,5 +0,0 @@
-/Users/jfolk1/Work/com.discovery.streamingvideoplayer/app/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/hidapi/src/hidapi/android/hid.o: \
-  /Users/jfolk1/Work/com.discovery.streamingvideoplayer/app/jni/SDL/src/hidapi/android/hid.cpp \
-  /Users/jfolk1/Work/com.discovery.streamingvideoplayer/app/jni/SDL/src/hidapi/android/../hidapi/hidapi.h
-
-/Users/jfolk1/Work/com.discovery.streamingvideoplayer/app/jni/SDL/src/hidapi/android/../hidapi/hidapi.h:
